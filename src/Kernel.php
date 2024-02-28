ee8e3f8d5de6a5a480d008fb3a2ba4b5926606a5
piotrzatorski <piotr.zat@gmail.com>
2024-02-28 23:10:07 +0100

    Implement and configure Whipo shop module
    Implemented a new Whipo shop module with the creation of AppController, Product entity, and necessary templates. Refactored namespace from 'App' to 'Whipo\Shop' across the project. Additionally, updated Dockerfile with new extensions and doctrine configurations have been adjusted accordingly.

--- src/Kernel.php
+++ src/Kernel.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App;
+namespace Whipo\Shop;
 
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
---
+++ src/Modules/Product/Domain/Entity/Product.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Whipo\Shop\Modules\Product\Domain\Entity;
+
+
+class Product
+{
+    private ?int $id = null;
+
+    private ?string $name = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+}
